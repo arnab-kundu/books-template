046943e2d490c5d585dc72568a15025a66b4837b
Arnab Dell <arnab.kundu1990@gmail.com>
2025-09-14 22:22:44 +0530

    Restart App3

--- app/index.tsx
+++ app/index.tsx
@@ -4,3 +4,26 @@ import "expo-dev-client";
 import App from "../src/App";
 
 export default registerRootComponent(App);
+
+
+
+
+// import SignIn from "@/src/auth/SignIn";
+// const  RootLayout =() => {
+//   return <SignIn />;
+// }
+// export default RootLayout;
+
+
+// import SignUp from "@/src/auth/SignUp";
+// const  RootLayout =() => {
+//   return <SignUp />;
+// }
+// export default RootLayout;
+
+
+// import OnboardingScreen from "@/src/pages/OnboardingScreen";
+// const  RootLayout =() => {
+//   return <OnboardingScreen />;
+// }
+// export default RootLayout;
